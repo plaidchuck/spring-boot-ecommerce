4d9ef0a5bd9d92ea6381fd98c3af70116b7afa06
plaidchuck <plaidchuck@gmail.com>
2025-03-03 18:49:55 -0500

    Updated pom.xml file to fish Lombok issues

--- src/main/java/com/luv2code/ecommerce/entity/Product.java
+++ src/main/java/com/luv2code/ecommerce/entity/Product.java
@@ -2,6 +2,8 @@ package com.luv2code.ecommerce.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
