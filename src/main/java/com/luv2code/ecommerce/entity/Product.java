42310ba26aae2129e95d0f0ba84823a9b36c475e
plaidchuck <plaidchuck@gmail.com>
2025-03-05 21:45:50 -0500

    Added rest config file to make GET/read only API calls

--- src/main/java/com/luv2code/ecommerce/entity/Product.java
+++ src/main/java/com/luv2code/ecommerce/entity/Product.java
@@ -2,8 +2,6 @@ package com.luv2code.ecommerce.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
